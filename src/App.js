8f893919f9315dfd140c3d54cb354b74de26b0da
ehsan rasool <ehsanrasool844@gmail.com>
2025-08-23 12:29:19 +0500

    tik tak toe

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TicTacToe from './components/TicTacToe/TicTacToe';
+
+
+function App() {
+  return (
+    <div className='app' >
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
